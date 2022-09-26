671a438ba3a9af699e7420639bbf76487fd20368
Çağrı KART <ckarte43@gmail.com>
2022-09-26 10:09:52 +0300

    commit

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,10 @@
 
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependecyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,12 +16,16 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinesModule());
+            });
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddSingleton<IProductService,ProductManager>();
-            builder.Services.AddSingleton<IProductDal,EfProductDal>();
+            //builder.Services.AddSingleton<IProductService,ProductManager>();
+            //builder.Services.AddSingleton<IProductDal,EfProductDal>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -33,6 +40,7 @@ namespace WebAPI
             }
 
             app.UseHttpsRedirection();
+
 
             app.UseAuthorization();
 
